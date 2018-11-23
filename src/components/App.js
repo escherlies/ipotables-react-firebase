16892271aab98d58af9c2e4fede5b7f4b6a6f782
Enrico Scherlies <e.scherlies@me.com>
2018-11-23 20:05:06 +0100

    navigaton

--- src/components/App.js
+++ src/components/App.js
@@ -2,8 +2,11 @@ import React, { Component } from 'react';
 import IpoModule from './IpoModule';
 import base from '../functions/base';
 import _ from 'lodash'
-import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { BrowserRouter, Route, Switch, Link } from 'react-router-dom'
 import IpoModulesList from './IpoModulesList';
+import ButtonColored from './ui/buttons/ButtonColored';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
 
@@ -23,8 +26,13 @@ class App extends Component {
 
     return (
       <BrowserRouter>
-        <div className="App">
+        <div className="App" style={{ padding: 8 }}>
           <div>IPO Tables!</div>
+
+          <Route path='/:nav' component={BackButton} />
+
+
+          <Route exact path='/' component={Home} />
 
 
           <Route exact path='/modules' render={() => <IpoModulesList modules={modules} readOnly={true} />} />
@@ -32,7 +40,7 @@ class App extends Component {
           <Switch>
             <Route exact path='/modules/add-new-module' render={() => <IpoModule things={things} />} />
             <Route exact path='/modules/:moduleKey/edit' render={({ match, history }) => <IpoModule moduleKey={match.params.moduleKey} things={things} module={_.get(modules, match.params.moduleKey)} />} />
-            <Route exact path='/modules/:moduleKey' render={({ match, history }) => <IpoModule moduleKey={match.params.moduleKey} things={things} readOnly={true} module={_.get(modules, match.params.moduleKey)} navigateToModule={() => history.push(`/modules/${match.params.moduleKey}/edit`) } />} />
+            <Route exact path='/modules/:moduleKey' render={({ match, history }) => <IpoModule moduleKey={match.params.moduleKey} things={things} readOnly={true} module={_.get(modules, match.params.moduleKey)} navigateToModule={() => history.push(`/modules/${match.params.moduleKey}/edit`)} />} />
           </Switch>
 
         </div>
@@ -42,3 +50,23 @@ class App extends Component {
 }
 
 export default App;
+
+
+
+const Home = props => (<div>
+  <Link to='/modules'>
+    <ButtonColored title={'Modules List'} />
+  </Link>
+</div>)
+
+
+const BackButton = props => <div style={{ width: 120, margin: 5 }}>
+  <ButtonColored
+    title='Back'
+    icon={<FontAwesomeIcon icon={faArrowLeft} />}
+    onClick={() => {
+      const path = props.location.pathname.split('/')
+      path.pop()
+      props.history.push(path.join('/'))
+    }}></ButtonColored>
+</div>
